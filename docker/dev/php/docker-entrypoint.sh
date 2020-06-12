636c503d6a5c57aac8953bc898c7da9da3de59b7
Insaf Fazleev <fazleev.in@yandex.ru>
2020-06-12 19:00:49 +0300

    Настроено окружение и создана база данных

---
+++ docker/dev/php/docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if [ -z "${PHP_DIR_OWNER}" ]; then
+    OWNER=root:www-data
+else
+    OWNER=${PHP_DIR_OWNER}:www-data
+fi
+
+if [ -z "${PHP_DIR_MODE}" ]; then
+    MODE=770
+else
+    MODE=${PHP_DIR_MODE}
+fi
+
+function check_and_create {
+    DIR="/var/php/$1"
+    if [ ! -d "${DIR}" ]; then
+        mkdir -p ${DIR}
+        chmod "${MODE}" "${DIR}"
+        chown "${OWNER}" "${DIR}"
+    fi
+}
+
+check_and_create tmp
+check_and_create sessions
+check_and_create upload
+check_and_create log
+
+if [ "$#" -ne 0 ]; then
+    exec "$@"
+fi
